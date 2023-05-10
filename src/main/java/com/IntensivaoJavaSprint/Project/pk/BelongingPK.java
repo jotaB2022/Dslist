be8bd68634d00c6f8d0378b3199921a86ef2efa1
jotaB <joaobatista1999estudo@gmail.com>
2023-05-10 00:47:04 -0300

    Domain Model, Entity Belonging e GameList

---
+++ src/main/java/com/IntensivaoJavaSprint/Project/pk/BelongingPK.java
@@ -0,0 +1,69 @@
+package com.IntensivaoJavaSprint.Project.pk;
+
+import java.util.Objects;
+
+import com.IntensivaoJavaSprint.Project.entites.Game;
+import com.IntensivaoJavaSprint.Project.entites.GameList;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPK {
+
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList gameList;
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	
+	public BelongingPK() {
+		
+	}
+
+	public BelongingPK(GameList gameList, Game game) {
+		super();
+		this.gameList = gameList;
+		this.game = game;
+		
+	}
+
+	public GameList getGameList() {
+		return gameList;
+	}
+
+	public void setGameList(GameList gameList) {
+		this.gameList = gameList;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, gameList);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPK other = (BelongingPK) obj;
+		return Objects.equals(game, other.game) && Objects.equals(gameList, other.gameList);
+	}
+
+	
+	
+	
+}
